c26ed794af17ac0e2d8794977b54f7f6a02fe066
Atul Singh <atulsingh25@example.com>
2026-02-17 12:43:04 +0530

    mm

--- farmer-customer-app/js/api.js
+++ farmer-customer-app/js/api.js
@@ -1,5 +1,5 @@
 // frontend/js/api.js
-const BASE_URL = "https://digital-mandi-backend.onrender.com/api";
+const BASE_URL = "https://digital-mandii-3.onrender.com/api";
 
 /* ================= HELPER ================= */
 async function safeFetch(url, options = {}) {
