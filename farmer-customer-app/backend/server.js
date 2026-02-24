e3bdff4a11dae160bae478c663088544e80daf83
Atul Singh <atulsingh25@example.com>
2026-02-24 20:35:49 +0530

    mm

--- farmer-customer-app/backend/server.js
+++ farmer-customer-app/backend/server.js
@@ -51,4 +51,4 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));git 
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
