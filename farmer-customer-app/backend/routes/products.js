144c9c2cf68e04ed16f8d492923412842178e7cf
Atul Singh <atulsingh25@example.com>
2026-02-24 11:37:48 +0530

    mm

--- farmer-customer-app/backend/routes/products.js
+++ farmer-customer-app/backend/routes/products.js
@@ -1,6 +1,7 @@
 import express from "express";
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 import {
   addProduct,
@@ -12,22 +13,34 @@ import {
 
 const router = express.Router();
 
-/* ================= MULTER ================= */
+/* ================= MULTER CONFIG ================= */
+
+// ensure uploads folder exists
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+}
+
 const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+});
 
 /* ================= ROUTES ================= */
 
 router.get("/", getProducts);
 router.get("/:id", getSingleProduct);
+
 router.post("/", upload.single("productImage"), addProduct);
 router.put("/:id", upload.single("productImage"), updateProduct);
+
 router.delete("/:id", deleteProduct);
 
 export default router;
